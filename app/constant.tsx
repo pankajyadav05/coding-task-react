799a3960aa7942ff56663542d5d4cd289babbcee
VishwaAtgit <vishwashree.challa@outlook.com>
2024-01-06 18:14:11 +0530

    Implement increment, decrement and AddIdOff logic

---
+++ app/constant.tsx
@@ -0,0 +1,6 @@
+// constants.js
+
+export const INCREMENT_AMOUNT_ERROR_MESSAGE = "Please enter a value greater than 0.";
+export const ADD_IF_ODD_ERROR_MESSAGE = "Sorry!! Updates are permitted only for odd counts.";
+export const ADD_AMOUNT = "Add Amount";
+export const ADD_IF_ODD = "Add If Odd";
